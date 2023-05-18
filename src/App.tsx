7cb19db938c01e33abe568c37552e174a3e326e9
JhonathaNunes <jhonahtasilveira@gmail.com>
2023-05-18 17:47:44 -0300

    Style news article and content container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css'
 import ColumnistsSection from './components/ColumistsSection'
 import Header from './components/Header'
+import NewsArticle from './components/NewsArticle';
 
 function App() {
   const columns = [
@@ -30,10 +30,42 @@ function App() {
     },
   ];
 
+  const relateds = [
+    {
+      title: "PEC da Anistia se choca com decisões de STF e TSE sobre inclusão de mulheres e negros",
+      link: "https://www1.folha.uol.com.br/poder/2023/05/ministerio-da-igualdade-racial-chama-pec-da-anistia-de-retrocesso-inadmissivel.shtml"
+    },
+    {
+      title: "PEC da Anistia se choca com decisões de STF e TSE sobre inclusão de mulheres e negros",
+      link: "https://www1.folha.uol.com.br/poder/2023/05/ministerio-da-igualdade-racial-chama-pec-da-anistia-de-retrocesso-inadmissivel.shtml"
+    },
+  ]
+
   return (
     <>
       <Header />
-      <ColumnistsSection columnistCards={columns} />
+      <main>
+        <ColumnistsSection columnistCards={columns} />
+        <div className='news-block'>
+          <section className='news-content'>
+            <NewsArticle
+              headlineKicker='PEC DA ANISTIA'
+              kickerLink='https://www1.folha.uol.com.br/poder/folhajus/'
+              headlineSize='h1'
+              href='https://www1.folha.uol.com.br/poder/2023/05/pgr-sob-lula-apoia-investigacao-contra-bolsonaro-que-antes-considerava-ilegal-e-nula.shtml'
+              headline='PGR sob Lula apoia investigação contra Bolsonaro que antes considerava ilegal e nula'
+              subheadline='Procuradoria chefiada por Augusto Aras se manifestou por arquivamento de apuração várias vezes ao longo de 2022'
+              relateds={relateds}
+              />
+          </section>
+          <aside className='aside-content'>
+
+          </aside>
+        </div>
+        
+        
+      </main>
+      
     </>
   )
 }
--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -1,39 +1,69 @@
-import { BsArrowReturnRight } from "react-icons/bs"
+import { BsArrowReturnRight } from "react-icons/bs";
+import { BiBookmark } from "react-icons/bi";
+import { IoMdShare } from "react-icons/io";
+import "../assets/styles/news-article.scss";
 
-type headlineSize = "h1"|"h2"|"h3"
+type headlineSize = "h1"|"h2"|"h3";
+
+interface RelatedArticle {
+  title: string;
+  link: string;
+}
 
 interface NewsArticleProps {
+  headlineKicker: string;
+  kickerLink: string;
   headlineSize: headlineSize;
+  href: string;
   headline: string;
-  subheader?: string;
-  summaries: string[];
-}
+  subheadline?: string;
+  relateds: RelatedArticle[];
+};
 
 const returnCorrectHeading = (headlineSize: headlineSize, headline: string) => {
   if (headlineSize === "h2") {
-    return <h2>{headline}</h2>
+    return <h2 className="headline h2">{headline}</h2>
   }
 
-  if (headline === "h3") {
-    return <h3>{headline}</h3>
+  if (headlineSize === "h3") {
+    return <h3 className="headline h3">{headline}</h3>
   }
 
-  return <h1>{headline}</h1>
+  return <h1 className="headline h1">{headline}</h1>
 }
 
-const NewsArticle = ({ headlineSize, headline, subheader, summaries }: NewsArticleProps ) => {
+const NewsArticle = ({
+  headlineKicker,
+  kickerLink,
+  headlineSize,
+  href,
+  headline,
+  subheadline,
+  relateds
+}: NewsArticleProps) => {
   return (
-    <article>
+    <article className="article">
+      <div className="headline-head">
+        <a href={kickerLink}>
+          <h3 className="headline-kicker">
+            {headlineKicker}
+          </h3>
+        </a>
+        <IoMdShare />
+        <BiBookmark />
+      </div>
+      <a href={href}>
         {returnCorrectHeading(headlineSize, headline)}
-        { subheader && <h2>{subheader}</h2> }
-        <ul>
-          { summaries.map((summay, index) => {
-            <li key={index}>
-              <BsArrowReturnRight />
-              {summay}
-            </li>
-          })}
-        </ul>
+        { subheadline && <p className="subheadline">{subheadline}</p> }
+      </a>
+      <ul className="summary-list">
+        { relateds.map((related, index) => 
+          <li key={index}>
+            <BsArrowReturnRight />
+            <a href={related.link}><h2>{related.title}</h2></a>
+          </li>
+        )}
+      </ul>
     </article>
   )
 }
